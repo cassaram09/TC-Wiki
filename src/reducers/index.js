1d98cf0ea145ffc127437c9367a399919c1dd64e
Amelie Oller <amelieoller@gmail.com>
2018-07-11 16:34:53 -0700

    Create user and loading reducers and root reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import user from './userReducer';
+import loading from './loadingReducer';
+
+const rootReducer = combineReducers({
+	user,
+	loading
+});
+
+export default rootReducer;
