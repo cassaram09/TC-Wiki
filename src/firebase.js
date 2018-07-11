a01d9bd175f37961c676240b33c43e46aa30abae
Amelie Oller <amelieoller@gmail.com>
2018-07-11 16:22:43 -0700

    Set up firebase with github auth

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+var config = {
+	apiKey: 'AIzaSyBl3fOB975a1t6YuXf5XJjZaUemY5Kcb3Q',
+	authDomain: 'tc-wiki-faf85.firebaseapp.com',
+	databaseURL: 'https://tc-wiki-faf85.firebaseio.com',
+	projectId: 'tc-wiki-faf85',
+	storageBucket: 'tc-wiki-faf85.appspot.com',
+	messagingSenderId: '885291438266'
+};
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const githubProvider = new firebase.auth.GithubAuthProvider();
+githubProvider.addScope('repo');
