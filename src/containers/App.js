2472c3b180aec20cd8f615b47a4254783068591b
Amelie Oller <amelieoller@gmail.com>
2018-07-11 16:31:56 -0700

    Add redux store, routing and an app component

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	constructor() {
+		super();
+	}
+
+	render() {
+		return <div className="App" />;
+	}
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<div>App</div>, document.getElementById('root'));
+// Components
+import App from './containers/App';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={App} />
+			</Switch>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
+);
 registerServiceWorker();
