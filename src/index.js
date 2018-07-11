f3a301f12a8f79215842d3c4d9001fe6259fec92
Amelie Oller <amelieoller@gmail.com>
2018-07-11 16:37:24 -0700

    Add loading/authentication to index.js and login component route

--- src/index.js
+++ src/index.js
@@ -4,21 +4,32 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 // Components
 import App from './containers/App';
+import Loading from './containers/Loading';
+import Authentication from './containers/Authentication';
+import Login from './containers/Login';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
 		<BrowserRouter>
-			<Switch>
-				<Route exact path="/" component={App} />
-			</Switch>
+			<Loading>
+				<Switch>
+					<Route exact path="/login" component={Login} />
+					<Authentication>
+						<Switch>
+							<Route exact path="/" component={App} />
+						</Switch>
+					</Authentication>
+				</Switch>
+			</Loading>
 		</BrowserRouter>
 	</Provider>,
 	document.getElementById('root')
