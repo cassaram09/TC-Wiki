b021f6996d3587dd9f62ae4a182ee116a96b8037
Amelie Oller <amelieoller@gmail.com>
2018-07-19 13:25:52 -0700

    Add page aaq

---
+++ src/components/Aaq.js
@@ -0,0 +1,160 @@
+import React from 'react';
+
+const Aaq = () => (
+	<div>
+		<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+			<h1>AAQ</h1>
+		</div>
+		<h3>Tips for AAQ</h3>
+		<ul>
+			<li>Watch Slack</li>
+			<li>Watch AAQ</li>
+			<li>Tools to consider:</li>
+			<ul>
+				<li>Using Better Snap Tool</li>
+				<li>Using snippet manager (Dash), good snippets to put in:</li>
+				<ul>
+					<li>join me here</li>
+					<li>1:1 support link</li>
+				</ul>
+			</ul>
+			<li>Tips for Timing:</li>
+			<ul>
+				<li>Set a timer for 10 min half way point?</li>
+			</ul>
+			<li>Tips for Zoom:</li>
+			<ul>
+				<li>Make the link automatically copy when starting a screenshare</li>
+			</ul>
+			<li>How to best get to the solutions when helping a student:</li>
+			<ul>
+				<li>Click on the solution branch</li>
+				<li>Go to pull requests to see students solutions in one place</li>
+				<li>or maybe have your labs on your machine so you're ready to jump into that lab</li>
+				<li>maybe clone down students code</li>
+			</ul>
+			<li>What to do when it's not busy:</li>
+			<ul>
+				<li>Resolve old questions</li>
+			</ul>
+			<li>
+				suggest <a href="https://learn.co/study-groups">study groups</a> for students to go to
+			</li>
+			<li>add a note to a student (at the bottom of chat)</li>
+			<li>what to do when aaq is down:</li>
+			<ul>
+				<li>send an @here message in student slack and tell students that they should ask questions there</li>
+			</ul>
+			<li>what to do when Slack is down:</li>
+			<ul>
+				<li>Create a group google hangout as a replacement</li>
+			</ul>
+		</ul>
+		<ul>
+			<li>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://github.com/flatiron-labs/technical-coach-resources/blob/master/dispatch-cheatsheet.md"
+				>
+					2 Minute Tid-Bits Videos
+				</a>
+				(IDE Editor Fundamentals, Funky Function, The Difference Between Parameters and Arguments, How to Read
+				JS Documentation, Ruby Each Method, Understanding Argument Errors)
+			</li>
+
+			<li>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://chrome.google.com/webstore/detail/le3/hjjhpafjpkkjbdchnaeikofponobhngc"
+				>
+					Learn Expert Extension
+				</a>
+			</li>
+
+			<li>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://docs.google.com/document/d/1QRAyN5lXPhJIlSeCavhWXzX92DXD9rL4pLurbvef_nY/edit?ts=5aa7fae2"
+				>
+					Student Resources Sheet
+				</a>
+			</li>
+
+			<li>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="http://help.learn.co/instructional-support/receiving-course-support/who-are-the-section-leads"
+				>
+					Who are the Section Leads?
+				</a>
+			</li>
+
+			<li>
+				<a target="_blank" rel="noopener noreferrer" href="https://theflatironschool.typeform.com/to/B9BrgH">
+					1:1 Project Support Link
+				</a>
+			</li>
+		</ul>
+		<h4>Starting new Shift in the Morning</h4>
+		<pre>
+			<code>Tech Coaches are back for the day, let us know if you still need help with this.</code>
+		</pre>
+		<h4>Resolving Questions</h4>
+		<pre>
+			<code>
+				Hi there! We noticed that you haven't touched this question in the last 8 hours so we're going to mark
+				it as resolved for now. Please feel free to ask another question if you get stuck!
+			</code>
+		</pre>
+		<pre>
+			<code>Hi! Do you still need help with this?</code>
+		</pre>
+		<pre>
+			<code>
+				Thanks for using AAQ, based on the message I am assuming we can close this question. If you need any
+				more help please don't hesitate to ask!
+			</code>
+		</pre>
+		<pre>
+			<code>
+				Great job! You've got this! Also don't forget to resolve the question, we'd love to get feedback from
+				you!
+			</code>
+		</pre>
+		<h2>Sync</h2>
+		<ul>
+			<li>
+				Give a “bite-size assignment” - challenge student to do something concrete toward reaching their goal
+				- may help student focus and not be overwhelmed
+			</li>
+			<li>Point out something they might need to go back and read again</li>
+			<li>
+				<a href="http://help.learn.co/resource-guide-for-most-asked-about-labs">
+					Resource guides for most asked about labs!
+				</a>
+			</li>
+			<li>
+				<a href="https://github.com/flatiron-labs/technical-coach-resources/blob/master/dispatch-cheatsheet.md#2-minute-tid-bits">
+					Two Minute Tidbits videos to give to students
+				</a>
+			</li>
+			<li>
+				<a href="https://instruction.learn.co/student/video_lectures#/">Video Lectures</a>
+			</li>
+		</ul>
+		<h2>Dispatch</h2>
+		<ul>
+			<li>Use the resources! If TCs are idle add students to the queue! You are the team manager!</li>
+			<li>
+				If you are going to take a student - take from the bottom! Keep it “fair and square” (those are the
+				students that have been waiting the longest)
+			</li>
+		</ul>
+	</div>
+);
+
+export default Aaq;
