3dc3169178aacace92eb21ccfd13dd073590071b
Amelie Oller <amelieoller@gmail.com>
2018-07-19 13:24:55 -0700

    Add Page 1:1s

---
+++ src/components/OneOnOnes.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const OneOnOnes = () => {
+	return (
+		<div>
+			<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+				<h1>1:1s</h1>
+			</div>
+
+			<div className="list-group">
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://github.com/flatiron-labs/technical-coach-resources/blob/master/support_coach/guide.md"
+					className="list-group-item list-group-item-action flex-column align-items-start "
+				>
+					<div className="d-flex w-100 justify-content-between">
+						<h5 className="mb-1">1:1 Support Coach Guide</h5>
+					</div>
+				</a>
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://docs.google.com/presentation/d/1vKWF3bsL3fs0aG5lfO6YetTM-0cGd8p45mCc927R1TI/edit#slide=id.p"
+					className="list-group-item list-group-item-action flex-column align-items-start "
+				>
+					<div className="d-flex w-100 justify-content-between">
+						<h5 className="mb-1">1:1 Support Coach Slide deck</h5>
+					</div>
+				</a>
+			</div>
+			<img src="../shared/images/youtube.svg" alt="" />
+		</div>
+	);
+};
+
+export default OneOnOnes;
