24c58e9340d3d0f06e4ad83dabe6a1c35cdd76ba
Amelie Oller <amelieoller@gmail.com>
2018-07-19 13:37:34 -0700

    Create page teaminfo

---
+++ src/components/TeamInfo.js
@@ -0,0 +1,121 @@
+import React from 'react';
+
+const TeamInfo = () => (
+	<div className="row">
+		<div className="col-4">
+			<h3>TC Teams</h3>
+			<div className="table-responsive">
+				<table className="table table-striped table-sm">
+					<thead>
+						<tr>
+							<th>Erika</th>
+							<th>Dustin</th>
+							<th>Matt</th>
+							<th>Amelie</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Aysan</td>
+							<td>David</td>
+							<td>Ashley</td>
+							<td>Jon</td>
+						</tr>
+						<tr>
+							<td>Dalia</td>
+							<td>James</td>
+							<td>Brad</td>
+							<td>Katerina</td>
+						</tr>
+						<tr>
+							<td>Jennifer</td>
+							<td>Justin</td>
+							<td>Ian</td>
+							<td>Ben</td>
+						</tr>
+						<tr>
+							<td>Sophia</td>
+							<td>Kevin</td>
+							<td>Josh</td>
+							<td>Chris</td>
+						</tr>
+						<tr>
+							<td>Nicole</td>
+							<td>Nick</td>
+							<td>Z</td>
+							<td>Seth</td>
+						</tr>
+						<tr>
+							<td>Rochel</td>
+							<td>Tyler</td>
+							<td>PJ</td>
+							<td />
+						</tr>
+						<tr>
+							<td />
+							<td>Mohammed</td>
+							<td />
+							<td />
+						</tr>
+					</tbody>
+				</table>
+				TCL (Technical Coach Lead), TC (Technical Coach), TCF (Technical Coach Fellow)
+			</div>
+		</div>
+		<div className="col-8">
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="http://help.learn.co/instructional-support/receiving-course-support/who-are-the-section-leads"
+			>
+				<h3>Section Leads</h3>
+			</a>
+
+			<div className="table-responsive">
+				<table className="table table-striped table-sm">
+					<thead>
+						<tr>
+							<th>Section</th>
+							<th>Lead</th>
+							<th>Email</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td>Pre-CLI (Intro to Ruby - HTML/CSS)</td>
+							<td>Nancy Noyes</td>
+							<td>nancy.noyes@flatironschool.com</td>
+						</tr>
+						<tr>
+							<td>CLI (Procedural Ruby to Object Oriented Ruby)</td>
+							<td>Kenlyn Terai</td>
+							<td>kenlyn@flatironschool.com</td>
+						</tr>
+						<tr>
+							<td>Sinatra</td>
+							<td>Howard DeVennish</td>
+							<td>howard@flatironschool.com</td>
+						</tr>
+						<tr>
+							<td>Rails</td>
+							<td>Luisa Scavo</td>
+							<td>luisa@flatironschool.com</td>
+						</tr>
+						<tr>
+							<td>JavaScript</td>
+							<td>Alice Balbuena</td>
+							<td>alice@flatironschool.com</td>
+						</tr>
+						<tr>
+							<td>React and Redux</td>
+							<td>Alice Balbuena</td>
+							<td>alice@flatironschool.com</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+);
+
+export default TeamInfo;
