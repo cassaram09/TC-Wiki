58c99e93cbdeb8a2eca5f9977d1c8b668ef74aad
Amelie Oller <amelieoller@gmail.com>
2018-07-24 12:53:12 -0700

    Update virtual box component list

--- src/components/VirtualBox.js
+++ src/components/VirtualBox.js
@@ -6,13 +6,17 @@ const VirtualBox = () => (
 		<div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 			<h1>VirtualBox</h1>
 		</div>
-		<ul>
-			<li>
-				<a href="https://github.com/flatiron-labs/technical-coach-resources/blob/master/virtual-box-setup-errors-and-resources.md#tutorial-how-to-enable-vt-x">
-					Link to VB troubleshooting repo
-				</a>
-			</li>
-		</ul>
+		<div className="btn-group">
+			<a
+				target="_blank"
+				rel="noopener noreferrer"
+				href="https://github.com/flatiron-labs/technical-coach-resources/blob/master/virtual-box-setup-errors-and-resources.md#tutorial-how-to-enable-vt-x"
+				className="btn btn-sm btn-outline-secondary mb-3"
+			>
+				VB Troubleshooting Repo
+			</a>
+		</div>
+
 		<h3>Resizing HD in VirtualBox</h3>
 		<ReactPlayer url="https://www.youtube.com/watch?v=q-9r-UGunus&index=1&list=PLi0yUl9brD2OR7dJM_FhoC3EGakbkEljx&t=3s" />
 	</div>
